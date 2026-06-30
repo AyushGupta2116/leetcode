052c87bdaf196f20d577de86e76ed10c79370b2b
Ayush Gupta <ayushgi2116@gmail.com>
2026-06-30 19:03:52 +0530

    Time: 56 ms (79.19%), Space: 42.2 MB (96.75%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -5,12 +5,12 @@ class Solution(object):
         nindex =1
         ans = [0] * n
         
-        for i in range(n):
-            if nums[i]<0:
-               ans[nindex] = nums[i]
+        for i in nums:
+            if i<0:
+               ans[nindex] = i
                nindex = nindex+2
             else:
-                ans[pindex]= nums[i]
+                ans[pindex]= i
                 pindex = pindex+2
 
         return ans
