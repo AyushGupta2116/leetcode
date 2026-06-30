78e9e93d1a75b4a21fb239016c0ccc70bc3adb3c
Ayush Gupta <ayushgi2116@gmail.com>
2026-07-01 01:20:19 +0530

    Time: 154 ms (7.65%), Space: 19.4 MB (28.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,7 +2,7 @@ class Solution(object):
     def maxProfit(self, nums):
         profit =0
         mini = nums[0]
-        for i in range(0,len(nums)):
+        for i in range(1,len(nums)):
             cost  = nums[i]-mini
             profit = max(profit,cost)
             mini = min(mini,nums[i])
