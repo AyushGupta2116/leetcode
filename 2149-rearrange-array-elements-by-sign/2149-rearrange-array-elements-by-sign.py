a0a7829d0036b1a7748283f0850d54feacb350d1
Ayush Gupta <ayushgi2116@gmail.com>
2026-06-30 19:01:57 +0530

    Time: 75 ms (60.65%), Space: 47.7 MB (33.5%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def rearrangeArray(self, nums):
+        n  = len(nums)
         pindex =0
         nindex =1
-        ans = [0] * len(nums)
+        ans = [0] * n
         
-        for i in range(0,len(nums)):
+        for i in range(n):
             if nums[i]<0:
                ans[nindex] = nums[i]
                nindex = nindex+2
