81a9895cd947c577762e9eb647dd50b69be1353d
Ayush Gupta <ayushgi2116@gmail.com>
2026-07-04 18:36:57 +0530

    Time: 15 ms (72.65%), Space: 12.6 MB (91.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,29 +1,17 @@
 class Solution(object):
     def isPalindrome(self, s):
-
         s = s.lower()
-        start = 0
-        end = len(s) - 1
-
-        def alphanumeric(ch):
-            if ('0' <= ch <= '9') or ('a' <= ch <= 'z'):
-                return True
-            return False
-
-        while start < end:
-
-            if not alphanumeric(s[start]):
-                start += 1
-                continue
-
-            if not alphanumeric(s[end]):
-                end -= 1
-                continue
-
-            if s[start] != s[end]:
+        n = len(s)
+        start = 0 
+        end = n-1
+        while start<end:
+            if not s[start].isalnum():
+                start+=1
+            elif not s[end].isalnum():
+                end-=1
+            elif s[start]!= s[end]:
                 return False
-
-            start += 1
-            end -= 1
-
+            else:
+                start+=1
+                end-=1
         return True
