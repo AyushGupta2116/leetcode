e56912ac780715c99ca90f5abe703989897af6ab
Ayush Gupta <ayushgi2116@gmail.com>
2026-08-16 01:13:06 +0530

    Time: 107 ms (9.73%), Space: 12.4 MB (91.98%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
         result = []
+        n = len(nums2)
 
         for j in nums1:
             found = False
             ans = -1
 
-            for i in range(len(nums2)):
+            for i in range(n):
                 if nums2[i] == j:
                     found = True
                 elif found and nums2[i] > j:
